8eb453377bf1b2b1a05557f4bf61723a1cebdebf
titoValen <valentin.tito@davinci.edu.ar>
2026-04-24 17:26:37 -0300

    Add header and footer components to main.js for improved layout

--- script/main.js
+++ script/main.js
@@ -1,7 +1,33 @@
 const app = Vue.createApp({
   data() {
-
+    return {
+      probando: "Esto es de prueba"
+    }
   }
 });
+
+app.component("com-header", {
+  template: 
+  `
+  <header>
+    <h1>Tienda de zapatillas</h1>
+    <button>
+      <figure>
+        <img src='../img/icon/cart.svg'>
+      </figure>
+      <span>Tu carrito</span>
+    </button>
+  </header>
+  `
+});
+
+app.component("com-footer", {
+  template:
+  `
+  <footer>
+    <h2>Es el footer funcionando</h2>
+  </footer>
+  `
+})
 
 app.mount("#app");
